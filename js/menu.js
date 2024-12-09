f46d7bda75b4a81c010d549dd3d4455dcd70494a
INF3RNOCAT <86680605+INF3RNOCAT@users.noreply.github.com>
2024-12-09 17:36:33 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,56 +1,56 @@
 const images = [
-    "../img/grilled-chicken/grilled-chicken.jpg",
-    "../img/grilled-chicken/grilled-chicken2.jpg",
-    "../img/grilled-chicken/grilled-chicken3.jpg"
+    "./img/grilled-chicken/grilled-chicken.jpg",
+    "./img/grilled-chicken/grilled-chicken2.jpg",
+    "./img/grilled-chicken/grilled-chicken3.jpg"
 ]
 
 const grilledchicken = [
-    "../img/grilled-chicken/grilled-chicken.jpg",
-    "../img/grilled-chicken/grilled-chicken2.jpg",
-    "../img/grilled-chicken/grilled-chicken3.jpg"
+    "./img/grilled-chicken/grilled-chicken.jpg",
+    "./img/grilled-chicken/grilled-chicken2.jpg",
+    "./img/grilled-chicken/grilled-chicken3.jpg"
 ]
 
 const bakedsalmon = [
-    "../img/baked-salmon/baked-salmon.jpg",
-    "../img/baked-salmon/baked-salmon2.jpg",
-    "../img/baked-salmon/baked-salmon3.jpg"
+    "./img/baked-salmon/baked-salmon.jpg",
+    "./img/baked-salmon/baked-salmon2.jpg",
+    "./img/baked-salmon/baked-salmon3.jpg"
 ]
 
 const beefstirfry = [
-    "../img/beef-stir-fry/beef-stir-fry.jpg",
-    "../img/beef-stir-fry/beef-stir-fry2.jpg",
-    "../img/beef-stir-fry/beef-stir-fry3.jpg"
+    "./img/beef-stir-fry/beef-stir-fry.jpg",
+    "./img/beef-stir-fry/beef-stir-fry2.jpg",
+    "./img/beef-stir-fry/beef-stir-fry3.jpg"
 ]
 
 const shrimptacos = [
-    "../img/shrimp-tacos/shrimp-tacos.jpg",
-    "../img/shrimp-tacos/shrimp-tacos2.jpg",
-    "../img/shrimp-tacos/shrimp-tacos3.jpg"
+    "./img/shrimp-tacos/shrimp-tacos.jpg",
+    "./img/shrimp-tacos/shrimp-tacos2.jpg",
+    "./img/shrimp-tacos/shrimp-tacos3.jpg"
 ]
 
 const chickenalfredo = [
-    "../img/chicken-alfredo/chicken-alfredo.jpg",
-    "../img/chicken-alfredo/chicken-alfredo2.jpg",
-    "../img/chicken-alfredo/chicken-alfredo3.jpg"
+    "./img/chicken-alfredo/chicken-alfredo.jpg",
+    "./img/chicken-alfredo/chicken-alfredo2.jpg",
+    "./img/chicken-alfredo/chicken-alfredo3.jpg"
 ]
 
 const spaghettibolognese = [
-    "../img/spaghetti-bolongnese/spaghetti-bolognese.jpg",
-    "../img/spaghetti-bolongnese/spaghetti-bolognese2.jpg",
-    "../img/spaghetti-bolongnese/spaghetti-bolognese3.jpg"
+    "./img/spaghetti-bolongnese/spaghetti-bolognese.jpg",
+    "./img/spaghetti-bolongnese/spaghetti-bolognese2.jpg",
+    "./img/spaghetti-bolongnese/spaghetti-bolognese3.jpg"
 ]
 
 const roastbeef = [
-    "../img/roast-beef/roast-beef.jpg",
-    "../img/roast-beef/roast-beef2.jpg",
-    "../img/roast-beef/roast-beef3.jpg"
+    "./img/roast-beef/roast-beef.jpg",
+    "./img/roast-beef/roast-beef2.jpg",
+    "./img/roast-beef/roast-beef3.jpg"
 ]
 
 
 const borgir = [
-    "../img/borgir/borgir.jpg",
-    "../img/borgir/borgir2.jpg",
-    "../img/borgir/borgir3.jpg"
+    "./img/borgir/borgir.jpg",
+    "./img/borgir/borgir2.jpg",
+    "./img/borgir/borgir3.jpg"
 ]
 
 var index = 1;
